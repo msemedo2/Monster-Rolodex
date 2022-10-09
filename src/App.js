86220474ed13fb3fecb9a95a6152b90f6fc867d3
unknown <miguel.semedo.2@outlook.com>
2022-10-09 16:02:42 +0100

    Search and Filter Functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
 		this.state = {
 			monsters: [],
+			searchField: '',
 		};
 	}
 
@@ -26,9 +27,24 @@ class App extends Component {
 	}
 
 	render() {
+		const filteredMonsters = this.state.monsters.filter((monster) => {
+			return monster.name.toLowerCase().includes(this.state.searchField);
+		});
+
 		return (
 			<div className="App">
-				{this.state.monsters.map((monster) => {
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={(e) => {
+						const searchField = e.target.value.toLowerCase();
+						this.setState(() => {
+							return { searchField };
+						});
+					}}
+				/>
+				{filteredMonsters.map((monster) => {
 					return (
 						<div key={monster.id}>
 							<h1>{monster.name}</h1>
