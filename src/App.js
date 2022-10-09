e9ea1a4cade7f0e264a45f2a25eca136047e157d
unknown <miguel.semedo.2@outlook.com>
2022-10-09 18:38:33 +0100

    Add Styling

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
 				<SearchBox
 					onChangeHandler={onSearchChange}
 					placeholder="search monsters"
-					className={'search-box'}
+					className={'monsters-search-box'}
 				/>
 				<CardList monsters={filteredMonsters} />
 			</div>
