e0d77d20e109d92d80b8f9617cc7f35163891a2b
unknown <miguel.semedo.2@outlook.com>
2022-10-09 12:12:26 +0100

    Fetch monster data from external source

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-	return <div className="App"></div>;
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) =>
+				this.setState(
+					() => {
+						return { monsters: users };
+					},
+					() => {
+						console.log(this.state);
+					}
+				)
+			);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.state.monsters.map((monster) => {
+					return (
+						<div key={monster.id}>
+							<h1>{monster.name}</h1>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
